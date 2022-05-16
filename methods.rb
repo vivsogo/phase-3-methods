9cfc1e774ac32988178eb9651d594576233ab342
Vivian Gomez <viviangomez0@gmail.com>
2022-05-16 11:52:36 -0400

    commit tree 2ddfe239b4a0020000a7a6162273bb6e7c45cc9b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,19 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name ="Naureen") 
+    puts "Hello, #{name}!"
+end 
+
+def greet_with_default (name)
+    if name = "Naureen"
+        return "Hello, #{name}!"
+    else
+    "Hello, programmer!"
+end
+
+def add (2,7)
+    return 2 + 7
+end
